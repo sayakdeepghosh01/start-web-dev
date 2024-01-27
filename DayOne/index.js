1f402d93ee75a821d3d03c4607c014beb99fbe0c
sayakdeepghosh01 <sayakdeepghosh01@gmail.com>
2024-01-28 00:43:26 +0530

    js added

---
+++ DayOne/index.js
@@ -0,0 +1,30 @@
+// 
+//
+//static type
+//  and 
+//  dynamic type
+//  javascript is a dynamic type language
+// // it specify type before init
+// we store data in several ways in javascript- 
+// let, var and const
+//int a = 10; // this is the way we init value in static
+var value = 10; // here 
+console.log(typeof value);
+//primitive datatypes- Number, String, Boolean, null, symbol, Bigint, undefined
+// reference - any object like array, functions, objects
+
+//number-
+// -2,147,483,647 to 2,147,483,647 is the range of number in javascript
+
+var a = 10000;
+
+//String-
+
+var name = "Sssssss";
+
+// null- value less
+//boolean- true or false
+// undefined- not specify
+// symbol
+//Bigint- 
+
