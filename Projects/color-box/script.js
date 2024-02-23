f4397f658395cb857131c2952d9df0735bafbd10
sayakdeepghosh01 <sayakdeepghosh01@gmail.com>
2024-02-24 00:40:39 +0530

    cmt

---
+++ Projects/color-box/script.js
@@ -0,0 +1,16 @@
+const box1 = document.getElementById('box1');
+const box2 = document.getElementById('box2');
+const magic = document.getElementById('magic');
+
+
+magic.addEventListener('click', () => {
+    let zIndex1 = paarseInt(box1.style.zIndex);
+    let zIndex2 = parseInt(box2.style.zIndex);
+    
+
+    // let tempColor = box1.style.backgroundColor;
+    // box1.style.backgroundColor = box2.style.backgroundColor;
+    // box2.style.backgroundColor = tempColor;
+});
+// 
+
